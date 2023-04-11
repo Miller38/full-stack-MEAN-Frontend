71bc9225051ea1b01b4a8dd0185a2ea6b30d563f
Miller <[200~103901963+Miller38@users.noreply.github.com~>
2023-04-11 12:37:26 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShowComponent } from './pages/show/show.component';
+import { CreateComponent } from './pages/create/create.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { GenericFormComponent } from './components/generic-form/generic-form.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShowComponent,
+    CreateComponent,
+    EditComponent,
+    GenericFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
